11d97eefa43d390696f3e2d9d18ee443f40c1583
David Khachaturov <d77777k@gmail.com>
2020-06-19 11:40:28 +0100

    more boat changes

--- boat.lua
+++ boat.lua
@@ -1,15 +1,39 @@
 local Boat = Object:extend()
 
+Boat.img = love.graphics.newImage("gfx/boat.png")
+
+local _forward_decel = 8
+local _rot_decel = math.pi/4
+
+local _instant_rot_speed = math.pi/8
+local _instant_forward_speed = 10
+local _max_speed = 30
+
 function Boat:new()
-    self.pos = {x = w/2, y = h/2, rot = math.pi/3}
+    self.pos = {x = w/2, y = h, rot = -math.pi/2}
+    self.mov = {
+        forward_speed = 0,
+        rot_speed = 0,
+    }
+end
+
+function Boat:update(dt)
+    self.pos.rot = self.pos.rot + self.mov.rot_speed * dt
+    self.pos.x = self.pos.x + self.mov.forward_speed * math.cos(self.pos.rot) * dt
+    self.pos.y = self.pos.y + self.mov.forward_speed * math.sin(self.pos.rot) * dt
+
+    self.mov.forward_speed = math.max(0, self.mov.forward_speed - _forward_decel * dt)
+    self.mov.rot_speed = self.mov.rot_speed < 0 and math.min(0, self.mov.rot_speed + _rot_decel * dt) or math.max(0, self.mov.rot_speed - _rot_decel * dt)
+end
+
+---@param left boolean
+function Boat:paddle(left)
+    self.mov.rot_speed = self.mov.rot_speed + (left and -1 or 1) * _instant_rot_speed
+    self.mov.forward_speed = math.min(self.mov.forward_speed + _instant_forward_speed, _max_speed)
 end
 
 function Boat:draw()
-    love.graphics.push()
-    --love.graphics.rotate(self.pos.rot)
-    --love.graphics.translate(self.pos.x, self.pos.y)
-    love.graphics.rectangle("fill", self.pos.x, self.pos.y, 20, 120)
-    love.graphics.pop()
+    love.graphics.draw(Boat.img, self.pos.x, self.pos.y, self.pos.rot, 1, 1, Boat.img:getWidth()/2, Boat.img:getHeight()/2)
 end
 
 return Boat
--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ w, h = 640, 360
 love.graphics.setDefaultFilter("nearest", "nearest", 0)
 
 Object = require "lib/classic"
+local Camera = require 'lib/Camera'
 
 local Boat = require "boat"
 
@@ -10,17 +11,28 @@ local screen = love.graphics.newCanvas(w, h)
 local boat = Boat()
 
 function love.load()
-    
+    camera = Camera(w/2, h/2, w, h)
+    camera:setFollowLerp(0.2)
+    camera:setFollowLead(10)
 end
 
 function love.update(dt)
-
+    camera:update(dt)
+    boat:update(dt)
+    --camera:follow(boat.pos.x, boat.pos.y)
 end
 
 function love.draw()
     love.graphics.setCanvas(screen)
-    love.graphics.clear()
-    boat:draw()
+        love.graphics.clear()
+
+        camera:attach()
+            
+            boat:draw()
+        
+        camera:detach()
+        camera:draw()
+
     love.graphics.setCanvas()
 
     local cur_w, cur_h, _ = love.window.getMode()
@@ -34,5 +46,11 @@ end
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
+    end
+
+    if key == "z" then
+        boat:paddle(true)
+    elseif key == "c" then
+        boat:paddle(false)
     end
 end
