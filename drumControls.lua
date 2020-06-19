5ae07d58f43f600bac3a2d455e68fc3357eacfff
David Khachaturov <d77777k@gmail.com>
2020-06-19 11:50:07 +0100

    drumContr

---
+++ drumControls.lua
@@ -0,0 +1,13 @@
+local DrumControls = Object:extend()
+
+DrumControls.hand_pos = {x = 0, y = 0}
+
+function DrumControls.static_foo()
+    
+end
+
+function DrumControls:non_static_foo()
+
+end
+
+return DrumControls
