1007d25322269e52e4f0c494cffc87426710840c
Hymyviiksi <hymyviiksi@gmail.com>
2020-06-19 15:51:21 +0300

    Drumstick graphics.

--- drumControls.lua
+++ drumControls.lua
@@ -1,33 +1,37 @@
-local DrumControls = Object:extend()
+local t = Object:extend()
 
 love.mouse.setVisible(false)
 
-DrumControls.drumstick_pos = {x = w * 0.75, y = h * 0.5}
-DrumControls.drumstick_targetPos = {x = DrumControls.drumstick_pos.x, y = DrumControls.drumstick_pos.y}
-DrumControls.drumstick_lerp = 16
-DrumControls.drumstick_minPos = {x = w / 2 + 5, y = 0 + 5}
-DrumControls.drumstick_maxPos = {x = w - 5, y = h - 5}
-DrumControls.drumstick_idlePos = {}
---DrumControls.drumstick_img = love.graphics.newImage("gfx/boat.png")
+t.drumstick_pos = {x = w * 0.75, y = h * 0.5}
+t.drumstick_targetPos = {x = t.drumstick_pos.x, y = t.drumstick_pos.y}
+t.drumstick_lerp = 16
+t.drumstick_minPos = {x = w / 2 + 5, y = 0 + 5}
+t.drumstick_maxPos = {x = w - 5, y = h - 5}
+t.drumstick_idlePos = {}
+t.drumstick_img = love.graphics.newImage("gfx/hand.png")
 
-function DrumControls.update(dt)
+function t.update(dt)
     local mouseX = love.mouse.getX() / love.graphics.getWidth() * w
     local mouseY = love.mouse.getY() / love.graphics.getHeight() * h
     
-    --if (mouseX >= DrumControls.drumstick_minPos.x and mouseX <= DrumControls.drumstick_maxPos.x and
-    --    mouseY >= DrumControls.drumstick_minPos.y and mouseY <= DrumControls.drumstick_maxPos.y) then
+    --if (mouseX >= t.drumstick_minPos.x and mouseX <= t.drumstick_maxPos.x and
+    --    mouseY >= t.drumstick_minPos.y and mouseY <= t.drumstick_maxPos.y) then
 
-        DrumControls.drumstick_targetPos.x = math.max(math.min(mouseX, DrumControls.drumstick_maxPos.x), DrumControls.drumstick_minPos.x)
-        DrumControls.drumstick_targetPos.y = math.max(math.min(mouseY, DrumControls.drumstick_maxPos.y), DrumControls.drumstick_minPos.y)
+        t.drumstick_targetPos.x = math.max(math.min(mouseX, t.drumstick_maxPos.x), t.drumstick_minPos.x)
+        t.drumstick_targetPos.y = math.max(math.min(mouseY, t.drumstick_maxPos.y), t.drumstick_minPos.y)
     --end
 
-    DrumControls.drumstick_pos.x = lerp(DrumControls.drumstick_pos.x, DrumControls.drumstick_targetPos.x, DrumControls.drumstick_lerp * dt)
-    DrumControls.drumstick_pos.y = lerp(DrumControls.drumstick_pos.y, DrumControls.drumstick_targetPos.y, DrumControls.drumstick_lerp * dt)
+    t.drumstick_pos.x = lerp(t.drumstick_pos.x, t.drumstick_targetPos.x, t.drumstick_lerp * dt)
+    t.drumstick_pos.y = lerp(t.drumstick_pos.y, t.drumstick_targetPos.y, t.drumstick_lerp * dt)
 end
 
-function DrumControls.draw()
+function t.draw()
     love.graphics.setColor(1, 1, 1)
-    love.graphics.circle("fill", DrumControls.drumstick_pos.x, DrumControls.drumstick_pos.y, 10, 6)
+    love.graphics.push()
+    love.graphics.translate(-w/2, 0)
+    --love.graphics.circle("fill", t.drumstick_pos.x, t.drumstick_pos.y, 10, 6)
+    love.graphics.draw(t.drumstick_img, t.drumstick_pos.x, t.drumstick_pos.y, 0, 1, 1, 0, 0)
+    love.graphics.pop()
 end
 
-return DrumControls
+return t
