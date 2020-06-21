d00bf44b124ec0b9beb9957c655db5404ff4eb0e
Hymyviiksi <hymyviiksi@gmail.com>
2020-06-21 22:20:04 +0300

    Working on pause screen.

--- states/game.lua
+++ states/game.lua
@@ -21,6 +21,8 @@ local time_til_next_light = 0
 
 local paused = false
 
+local pauseFont
+
 function state:new()
 	return lovelyMoon.new(self)
 end
@@ -32,6 +34,8 @@ function state:load()
     camera:setFollowStyle('TOPDOWN_TIGHT')
 
     drumControls.init(player_boat)
+
+    pauseFont = love.graphics.newFont("PERTILI.TTF", 26, "mono")
 
     --for i=1, 100 do
     --    Obstacles.new(love.math.random(-w, w), love.math.random(-h, h), "rock")
@@ -117,8 +121,19 @@ function state:draw()
     love.graphics.draw(drumScreen, cur_w/2, 0, 0, cur_w/w, cur_h/h)
     love.graphics.setBlendMode('alpha')
 
+    
     if (paused) then
-        
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", 0, 0, love.graphics:getWidth(), love.graphics:getHeight())
+        love.graphics.setFont(pauseFont)
+        love.graphics.setColor(1, 1, 1)
+        local txt
+        txt = "GAME PAUSED"
+        love.graphics.print(txt, love.graphics.getWidth()/2, 50, 0, 1, 1, love.graphics.getFont():getWidth(txt)/2, 0)
+        txt = "[ESC] to continue."
+        love.graphics.print(txt, love.graphics.getWidth()/2, 100, 0, 1, 1, love.graphics.getFont():getWidth(txt)/2, 0)
+        txt = "[M] to return to main menu."
+        love.graphics.print(txt, love.graphics.getWidth()/2, 150, 0, 1, 1, love.graphics.getFont():getWidth(txt)/2, 0)
     end
 end
 
@@ -126,6 +141,9 @@ function state:keypressed(key, unicode)
     if key == "escape" then
         paused = not paused
         love.mouse.setGrabbed(not paused)
+    end
+    if paused and key == "m" then
+        lovelyMoon.switchState("game", "title")
     end
 end
 
